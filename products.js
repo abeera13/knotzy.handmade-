2a79c75f31615d44b8ef047447f3026deee175fe
abeera13 <r97847688@gmail.com>
2026-07-31 22:14:39 +0500

    Add files via upload

---
+++ products.js
@@ -0,0 +1,57 @@
+const products = [
+
+{
+id: "cool-bracelet",
+name: "Cool Bracelet",
+price: 299,
+category: "Bracelets",
+image: "images/Screenshot 2026-07-28 180429.png",
+description: `Level up your style with our handmade Cool Bracelet! ✨ Designed for both boys and girls, this trendy bracelet is lightweight, comfortable, and perfect for everyday wear. Handmade with love and made to match every style. Perfect for gifting, matching with friends, or treating yourself!`
+},
+
+{
+id: "cool-mint",
+name: "Cool Mint Bracelet",
+price: 320,
+category: "Bracelets",
+image: "images/Screenshot 2026-07-31 151229.png",
+description: "Mint theme with white glass, blue crackle beads, and a star charm. Fresh, cute, and matches your vibe."
+},
+
+{
+id: "waves",
+name: "Waves Bracelet",
+price: 320,
+category: "Bracelets",
+image: "images/Screenshot 2026-07-31 165754.png",
+description: `Ride the waves of style with our handmade Waves Bracelet! 🌊 Featuring soft blue, white, and pastel beads with a beautiful shell charm, this bracelet brings calm ocean vibes to every outfit. Lightweight, comfortable, and perfect for everyday wear or gifting. Handmade with love for anyone who loves the sea.`
+},
+
+{
+id: "pearl",
+name: "Pearl Bracelet",
+price: 400,
+category: "Bracelets",
+image: "images/product4.jpg",
+description: "Classic pearl bracelet."
+},
+
+{
+id: "phonecharm",
+name: "Phone Charm",
+price: 250,
+category: "Phone Charms",
+image: "images/product5.jpg",
+description: "Cute handmade phone charm."
+},
+
+{
+id: "flowerring",
+name: "Flower Ring",
+price: 180,
+category: "Bracelets",
+image: "images/product6.jpg",
+description: "Beautiful flower ring."
+}
+
+];
